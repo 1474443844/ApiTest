711f03bc3b637051a1ca28366a7a0d82ab361184
1474443844 <1474443844@qq.com>
2024-07-30 03:16:22 +0800

    新增更新下载进度条

--- app/src/main/java/cn/wantu/apitest/MainActivity.kt
+++ app/src/main/java/cn/wantu/apitest/MainActivity.kt
@@ -1,12 +1,12 @@
 package cn.wantu.apitest
 
 import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import androidx.activity.ComponentActivity
 import cn.wantu.apitest.data.ApiTestConfig
-import cn.wantu.apitest.dialog.DownloadProgressDialogFragment
 import com.google.gson.Gson
 import okhttp3.Request
 import okio.BufferedSink
@@ -23,7 +23,10 @@ class MainActivity : ComponentActivity() {
         val app = application as ApiApp
         val client = app.okHttpClient
         val handler = Handler(mainLooper)
-//        var dialog = DownloadProgressDialogFragment()
+        val progressDialog = ProgressDialog(this)
+        progressDialog.setMessage("Downloading...")
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+        progressDialog.setCancelable(false)
         thread {
             val request = Request.Builder()
                 .url("https://docs.wty5.cn/App/ApiTestConfig.json")
@@ -37,13 +40,14 @@ class MainActivity : ComponentActivity() {
             }else{
                 pi.versionCode.toLong()
             }
-            println(currentVersion)
-            println(json.versionCode)
             if (currentVersion < json.versionCode){
                 handler.post {
                     AlertDialog.Builder(this).setTitle("有新版本v${json.version}").setMessage(json.content)
                         .setPositiveButton("更新"){_,_->
                             thread {
+                                handler.post {
+                                    progressDialog.show()
+                                }
                                 try {
                                     // 更新
                                     val request2 = Request.Builder()
@@ -56,12 +60,33 @@ class MainActivity : ComponentActivity() {
                                         // 创建目标文件
                                         val file = File(getExternalFilesDir(null), fileName)
                                         // 下载并保存文件
-                                        response2.body?.source()?.let { source ->
-                                            file.outputStream().use { output ->
-                                                val sink: BufferedSink = output.sink().buffer()
-                                                sink.writeAll(source)
-                                                sink.close()
+                                        response2.body?.let { body ->
+                                            val contentLength = body.contentLength()
+                                            body.source().let { source ->
+                                                file.outputStream().use { output ->
+                                                    val sink = output.sink().buffer()
+                                                    val buffer = ByteArray(8192)
+                                                    var totalBytesRead: Long = 0
+                                                    var bytesRead: Int
+                                                    while (source.read(buffer).also { bytesRead = it } != -1) {
+                                                        sink.write(buffer, 0, bytesRead)
+                                                        totalBytesRead += bytesRead
+                                                        handler.post {
+                                                            progressDialog.progress = (100*totalBytesRead/contentLength).toInt()
+                                                        }
+                                                    }
+                                                    sink.close()
+                                                }
                                             }
+                                        }
+                                        handler.post {
+                                            progressDialog.dismiss()
+                                            AlertDialog.Builder(this).setTitle("下载成功")
+                                                .setMessage("File downloaded to: ${file.absolutePath}")
+                                                .setPositiveButton("安装"){ _, _ ->
+
+                                                }.show()
+
                                         }
                                         // 下载完成
                                         println("File downloaded to: ${file.absolutePath}")
